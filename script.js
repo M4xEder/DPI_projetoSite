31611782a34002617908335bcaccb1e54f425fd2
Max_Elder <100439684+M4xEder@users.noreply.github.com>
2022-06-10 18:51:36 -0300

    Script

---
+++ script.js
@@ -0,0 +1,16 @@
+/* botao menu com toggle ao da  click class ativar aparece  */
+const btnMobile = document.getElementById('btn-mobile');
+function toggleMenu(event){
+  if(event.type==='touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('ativar');
+  const ativar = nav.classList.contains('ativar')
+  event.currentTarget.setAttribute('aria-expanded', ativar);
+  if (ativar){ event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+}
+else{
+  event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+}
+}
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
